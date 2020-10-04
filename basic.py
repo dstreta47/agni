bab6602619364df73abb99941f63bf3b155432c0
dstreta47 <diazostretaoffcial@gmail.com>
2020-10-05 02:03:43 +0530

    second commit

--- basic.py
+++ basic.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 class InfoForm(FlaskForm):
-    breed= StringField("what breed are you!")
+    drink= StringField("What would you like to have sir/madam?")
     submit= SubmitField("submit")
 
 @app.route('/', methods= ['GET','POST'])
 def index():
-    breed= False 
+    drink= False 
     form= InfoForm()
     if form.validate_on_submit():
-        breed= form.breed.data
-        form.breed.data= ''
-    return render_template('index.html', form = form, breed = breed) 
+        drink= form.drink.data
+        form.drink.data= ''
+    return render_template('index.html', form = form, drink = drink) 
 
 
 if __name__ == '__main__':
